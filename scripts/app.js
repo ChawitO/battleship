52b7cab97383cac0f46e6128ab3b8c162a1c7418
Chawit.O <chawit.ocr@gmail.com>
2019-09-22 21:40:12 +0100

    Randomise AI ship placement

--- scripts/app.js
+++ scripts/app.js
@@ -15,7 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const board = document.querySelector('.board')
   const boardWidth = 10
   const friendlyOcean = []
-  const shipInPlay = []
+  const friendlyFleet = []
   const targetBoard = document.querySelector('.target-board')
   const enemyOcean = []
   const attempts = []
@@ -31,16 +31,18 @@ window.addEventListener('DOMContentLoaded', () => {
     new Ship('submarine', 3),
     new Ship('patrol boat', 2)
   ]
-  const enemyShipInPlay = [
-    new Ship('carrier', 5, [0, 1, 2, 3, 4]),
-    new Ship('battleship', 4, [10, 11, 12, 13]),
-    new Ship('destroyer', 3, [20, 21, 22]),
-    new Ship('submarine', 3, [30, 31, 32]),
-    new Ship('patrol boat', 2, [40, 41])
+  const enemyShips = [
+    new Ship('carrier', 5),
+    new Ship('battleship', 4),
+    new Ship('destroyer', 3),
+    new Ship('submarine', 3),
+    new Ship('patrol boat', 2)
   ]
+  const enemyFleet = []
 
-  // Generate the 10x10 board
+  // Generate the 10x10 boards
   generateBoard(friendlyOcean, board)
+  generateBoard(enemyOcean, targetBoard)
 
   // Rotate ship during placement
   document.addEventListener('keyup', (e) => {
@@ -60,7 +62,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // Add ship preview
     cell.addEventListener('mouseover', function() {
       const ship = ships[0]
-      if (invalidPlacement(ship)) return
+      if (invalidPlacement(this.index, ship, friendlyFleet)) return
 
       for (let j = 0; j < ship.size; j++) {
         friendlyOcean[getIndex(x, y, j)].classList.add('ghost')
@@ -70,7 +72,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // Remove ship preview
     cell.addEventListener('mouseout', function() {
       const ship = ships[0]
-      if (invalidPlacement(ship)) return
+      if (invalidPlacement(this.index, ship, friendlyFleet)) return
 
       for (let j = 0; j < ship.size; j++) {
         friendlyOcean[getIndex(x, y, j)].classList.remove('ghost')
@@ -80,72 +82,71 @@ window.addEventListener('DOMContentLoaded', () => {
     // Placing down ship
     cell.addEventListener('click', function() {
       const ship = ships[0]
-      if (invalidPlacement(ship)) return
+      if (invalidPlacement(this.index, ship, friendlyFleet)) return
 
-      for (let j = 0; j < ship.size; j++) {
-        const index = getIndex(x, y, j)
-        ship.pos.push(index)
-        friendlyOcean[index].classList.remove('ghost')
-        friendlyOcean[index].classList.add('ship')
-      }
-      console.log(`Placing ${ship.type}`)
-      shipInPlay.push(ships.shift())
+      placeShip(this.index, ships.shift(), friendlyOcean, friendlyFleet, vertical, 'player')
       if (!ships.length) phase = 'play'
     })
-
-    // Functions
-    function invalidPlacement(ship) {
-      const shipPositions = shipInPlay.map(ship => ship.pos).flat()
-      if (!ship) return true
-      // Exceed x (boardWidth) when x > 9
-      if (!vertical && x + ship.size > boardWidth) return true
-      // Exceed y (boardWidth) when y > 9
-      if (vertical && y + ship.size > boardWidth) return true
-      // If the tile is occupied
-      for (let j = 0; j < ship.size; j++) {
-        const index = getIndex(x, y, j)
-        if (shipPositions.includes(index)) return true
-      }
-    }
-
-    function getIndex(x, y, modif = 0,) {
-      vertical ? y += modif : x += modif
-      return (y * 10) + x
-    }
   })
 
   // AI ship placement logic
+  while (enemyShips.length) {
+    const ship = enemyShips.shift()
+    console.log(ship)
+    // Randomise index and orientation
+    let index = getRandomIndex()
+    const vert = Math.random() >= 0.5
+    while (invalidPlacement(index, ship, enemyFleet, vert)) {
+      index = getRandomIndex()
+    }
 
+    placeShip(index, ship, enemyOcean, enemyFleet, vert)
+  }
+  console.log(enemyFleet)
+
+  function placeShip(index, ship, ocean, fleet, vert = vertical, user) {
+    for (let j = 0; j < ship.size; j++) {
+      const pos = getIndex(ocean[index].x, ocean[index].y, j, vert)
+      console.log(ocean[index].y)
+      ship.pos.push(pos)
+      if (user === 'player') {
+        ocean[pos].classList.remove('ghost')
+        ocean[pos].classList.add('ship')
+      }
+    }
+    fleet.push(ship)
+  }
 
   // Attack logic
-  generateBoard(enemyOcean, targetBoard)
-
   enemyOcean.forEach(tile => tile.addEventListener('click', function() {
     if (phase !== 'play') return
     if (attempts.includes(this.index)) return
     console.log(this.index)
 
     attempts.push(this.index)
-    const hit = checkHit(this.index, enemyShipInPlay, enemyOcean)
+    const hit = checkHit(this.index, enemyFleet, enemyOcean)
     if (hit) {
       enemyOcean[this.index].classList.add('hit')
     } else {
       enemyOcean[this.index].classList.add('miss')
     }
+
+    // Do AI attack after the user's
+    enemyAttack()
   }))
 
   // AI attack logic
   // Math.random() for now
-  const attackIntervalId = setInterval(() => {
-    switch (phase) {
-      case 'play':
-        enemyAttack()
-        break
-      case 'finished':
-        clearInterval(attackIntervalId)
-        break
-    }
-  }, 1000)
+  // const attackIntervalId = setInterval(() => {
+  //   switch (phase) {
+  //     case 'play':
+  //       enemyAttack()
+  //       break
+  //     case 'finished':
+  //       clearInterval(attackIntervalId)
+  //       break
+  //   }
+  // }, 1000)
 
   let prevPos
   function enemyAttack() {
@@ -167,9 +168,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     enemyAttempts.push(index)
     console.log(index)
-    const cell = friendlyOcean[index]
-    cell.textContent = '*'
-    const hit = checkHit(index, shipInPlay, friendlyOcean)
+    const tile = friendlyOcean[index]
+    tile.textContent = '*'
+    const hit = checkHit(index, friendlyFleet, friendlyOcean)
     if (hit) prevPos = index
     if (!directions.length) prevPos = false
   }
@@ -185,6 +186,23 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  function invalidPlacement(pos, ship, fleet, vert = vertical) {
+    const x = pos % boardWidth
+    const y = Math.floor(pos / boardWidth)
+    const shipPositions = fleet.map(ship => ship.pos).flat()
+
+    if (!ship) return true
+    // Exceed x (boardWidth) when x > 9
+    if (!vert && x + ship.size > boardWidth) return true
+    // Exceed y (boardWidth) when y > 9
+    if (vert && y + ship.size > boardWidth) return true
+    // If the tile is occupied
+    for (let j = 0; j < ship.size; j++) {
+      const index = getIndex(x, y, j, vert)
+      if (shipPositions.includes(index)) return true
+    }
+  }
+
   function checkHit(pos, defenderShips, ocean) {
     const ship = defenderShips.find(ship => ship.pos.includes(pos))
     if (!ship) return
@@ -197,6 +215,14 @@ window.addEventListener('DOMContentLoaded', () => {
     return ship
   }
 
+  function getRandomIndex() {
+    return Math.floor(Math.random() * boardWidth ** 2)
+  }
+
+  function getIndex(x, y, modif = 0, vert = vertical) {
+    vert ? y += modif : x += modif
+    return (y * 10) + x
+  }
 
 })
 
